94530dd476c466620a6421349cbfacdf9640654b
WheeSci <summers@wcu.edu>
2017-08-16 14:46:54 -0400

    Update setupRun.ino
    Minor changes to accommodate cyclic square wave voltammetry

--- WheeStatE_7d/setupRun.ino
+++ WheeStatE_7d/setupRun.ino
@@ -18,22 +18,16 @@ void setupRun() {
         delay(500);
         digitalWrite(RED_LED,LOW);
       }
-
       if(starti == '&') {  // 38) {         //GUI transmits & character to start read
         startread = true;
         runState = true;
       }
         starti = 0;
     }
- 
   }       // end of while startRead == false loop
    
-
   //////////// mode ////////////////  
-
   Serial.print(",,Mode, ");   //double comma to get header out of data columns
-  // Serial.print(",");
- // readParam();
 while (Serial.available()<=0){ }
 delay(20);
   mode= Serial.parseInt();   
@@ -43,46 +37,25 @@ delay(20);
   Serial.print("mV init, ");
   while (Serial.available() <=0){ }
 delay(20);
-  //  Serial.print(",");
- 
-   /*      digitalWrite(RED_LED,HIGH);
-        delay(500);
-        digitalWrite(RED_LED,LOW);
-        delay(500);
-        digitalWrite(RED_LED,HIGH);
-        delay(500);
-        digitalWrite(RED_LED,LOW);  */
- // readParam(); 
-  mVinit=Serial.parseInt();
-//  mVinit=Serial.parseInt()-2000;     // 2000 added in GUI to transfer positive value
+   mVinit=Serial.parseInt();
   dInit = mVinit/10 + halfRes;       //digital value for pwm
   delay(20);  
-   /*    digitalWrite(RED_LED,HIGH);
-        delay(500);
-        digitalWrite(RED_LED,LOW);
-        delay(500);
-        digitalWrite(RED_LED,HIGH);
-        delay(500);
-        digitalWrite(RED_LED,LOW); */
-
+   
   ///////////final voltage///////
   Serial.print("mV Final, ");
     while (Serial.available() <=0){ }
 delay(20);
-  //  Serial.print(",");
-//  readParam();
-//  mVfnl=Serial.parseInt();
     mVfnl=Serial.parseInt();
-    //-2000;       // 2000 added to value in GUI
   dFnl = mVfnl/10 + halfRes;       //digital value for pwm
   delay(20);
-
+  if (dInit<dFnl) {    // moved from ramp to help wiht cyclic square wave
+  slopeDir = 1;           // slopeDir = 1 if positive slope
+  }
+evenOdd = false;
   ///////////scan rate///////
   Serial.print("mV/s, ");
     while (Serial.available() <=0){ }
 delay(20);
-  // Serial.print(",");
-//  readParam();
   scanRate = Serial.parseInt();     
   stepTime = 10000*pwm_step/scanRate;      // ms per step
   delay(20);
@@ -96,33 +69,24 @@ delay(20);
   Serial.print("gain, ");
   while (Serial.available() <=0){ }
 delay(20);
-  // Serial.print(",");
- // readParam();   
   dB = Serial.parseInt();                        //dB = gain in decibels
   if (dB >= 19) {
     gain0 = 0;
     gain1 = hiGain1[dB-19];
- //   gain1 = 2*gain1;  // added for new potentiometers
-  }
+   }
   else {
     gain1 = 26;
-  //  gain1 = 2*gain1;  // added for new potentiometers
     gain0 = loGain0[dB];
-    //gain0 = 2*gain0;  // added for new potentiometers
-  } 
+    } 
   res4 = resPot*(128-gain0)/128+resW;            // Resistor 4 in schematic, resistance from 50 Kohm pot0
   res5 = resPot*(gain1)/128+resW;                // Resistor 5, from pot1
   amplif = res6*res4/res5;                         // amplification factor
-
-
   delay(20);
 
   ///////////offset voltage///////
   Serial.print("offset, ");
   while (Serial.available() <=0){ }
 delay(20);
-  //  Serial.print(",");
-//  readParam();  
   dOff1 = Serial.parseInt();
   dOff= dOff1 + offAdj; //  GUI adds 165 to input value (half of 330 digital resolution) 
   // addition of adjustment here assumes that analog write is source of error
@@ -132,9 +96,7 @@ delay(20);
   /////////// delay 1 ////////////////
   Serial.print("initial delay, ");
   while (Serial.available() <=0){ }
-delay(20);
-  // Serial.print(",");
-//  readParam();  
+delay(20); 
   delay1=Serial.parseInt();          // global varaible for current gain
   delay(20);
 
@@ -142,8 +104,6 @@ delay(20);
   Serial.print("runs, ");
   while (Serial.available() <=0){ }
 delay(20);
-  //  Serial.print(",");
- // readParam();
   runs=Serial.parseInt();                
   delay(20);
 
@@ -151,9 +111,7 @@ delay(20);
   Serial.print("log delay, ");
   while (Serial.available() <=0){ }
 delay(20);
-  // Serial.print(",");
- // readParam();
-  runDelay=Serial.parseInt(); 
+ runDelay=Serial.parseInt(); 
   
   delay(20);
 
@@ -161,14 +119,12 @@ delay(20);
   Serial.println("read time");
   while (Serial.available() <=0){ }
 delay(20);
- // readParam();
-  readTime=Serial.parseInt();                
+ readTime=Serial.parseInt();                
   delay(20);
 //////////////////////////////////////  
    Serial.println("read_units");
   while (Serial.available() <=0){ }
 delay(20);
- // readParam();
   readUnits=Serial.parseInt();                
   delay(20);
   if(readUnits == 1){
@@ -177,10 +133,8 @@ delay(20);
   if (readUnits ==2){
     readTime = 60000*readTime;
   }
- // Serial.println("180");
- Serial.flush();
+  Serial.flush();
   status();      // check to see if % sent, if so, reset 
-//  Serial.println("182");
   /********************* print values for header ********************/
   Serial.print(",,");              // two commas to get headers out of data columns
   Serial.print(mode);
@@ -219,39 +173,3 @@ delay(20);
 
 }  
 /******************************  end setupRun *******************************/
-
-/****************************** read Paramaters ****************************/
-/*void readParam() {
-  digitalWrite(RED_LED,HIGH);
-  delay(500);
-  digitalWrite(RED_LED,LOW);*/
-/*  long place[6]={
-    100000,10000,1000,100,10,1};           
-  int incomingByte[6] = {
-    0,0,0,0,0,0          };      // serial read variable. each incoming byte (multiplied by the place)
-  int n=0;
-  param = 0;
-  delayMicroseconds(300);  
-  while (Serial.available()<= 0) {
-  }
-  while (Serial.available() > 0){  
-    incomingByte[n] = Serial.read()-48;
-    delay(2);
-    ++n;          
-  }
-
-  for(int n=0;n<6;++n){ 
-    param = param + (place[n]*(incomingByte[n]));  
-  }*/
- /* param = Serial.parseInt();
-  delay(500);
-  digitalWrite(RED_LED, HIGH);
-  delay(500);
-  digitalWrite(RED_LED, LOW);
-  delay(500);
-}          */
-
-
-
-
-
