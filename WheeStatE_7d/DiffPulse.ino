a906ad85d7200123dcdb1125dd926a130a5b38a7
WheeSci <summers@wcu.edu>
2017-08-16 14:31:57 -0400

    Update DiffPulse.ino
    Code added to support differential pulse voltammetery experiment

--- WheeStatE_7d/DiffPulse.ino
+++ WheeStatE_7d/DiffPulse.ino
@@ -8,23 +8,49 @@
 
 void diffPulse()  {
 
-  ///////// Stripping ////////////////////////////////////////////////
+ if (mode == cySq){
+    nThru = 2;
+  }else{
+    nThru = 1;
+  }
+  ///////// determine parameters /////////////////
+for(int p = 0; p<nThru; p++){
+    if(evenOdd == 0){
+    start = dInit;
+    }
+    else{
+      start = dFnl;
+    }
+    runDifPulse(start,slopeDir);
+   slopeDir =! slopeDir;   // toggle slopeDir
+  evenOdd =! evenOdd;
+  }
+  
+  openCircuit();    // go to open circuit
+ }
 
+///////////////////////////// run routine /////////////////
+void runDifPulse(int start, boolean dir){
   for (int dC = 0; dC <= nSteps; ++dC){  
    if (runState == true){
-      if (dInit < dFnl){
-      dSig = dInit + (dC*pwm_step);      // calculate digital Signal value
+     // if (dInit < dFnl){
+    if (slopeDir == true){
+      dSig = start + (dC*pwm_step);      // calculate digital Signal value
+      stepDir = HIGH;
+      downDir = LOW;
     }
     else {
-      dSig = dInit - (dC*pwm_step);
-    }
+      dSig = start - (dC*pwm_step);
+      stepDir = LOW;
+downDir = HIGH;
+}
     PWMWrite(signal_pin,pwmRes,dSig,pwmClock);
-    digitalWrite(pulse_pin,HIGH);       
+    digitalWrite(pulse_pin,stepDir);       
     delay(stepTime/2);  
 
     readVolts();
     readCurrent(true);
-    digitalWrite(pulse_pin,LOW);       
+    digitalWrite(pulse_pin,downDir);       
     delay(stepTime/2);  
     readCurrent(true);
     Serial.print(iMin);
@@ -41,10 +67,7 @@ void diffPulse()  {
 
     }
   }
-  openCircuit();    // go to open circuit
- }
-
-
+  }
 
   void cleanDepos(){
   //////Electrode cleaning ////////////////
