57dfed78e0d59d2af43e1f9a108851497d0a0283
WheeSci <summers@wcu.edu>
2017-08-16 12:09:17 -0400

    Update Open_Circuit.ino
    Kill switch option incorporated.  To use with kill switch hardware, comment out lines 5-26, un-comment lines 27 through 32.  Kill switch circuit board can be purchased from OSH Park.  Go to https://oshpark.com/shared_projects and search for killSwitch2Gerbers by SmokyMtSci, shared Aug 16, 2017 at 15:55.  Circuit uses 500 ohm chip resistor (603 package) and solid state relay (IXYS- CPC1017NTR).   The board has seven wire pads. Cut red wire from aligator clips and solder each to one of wire pads on upper right labelled CE.  Cut green wire from alligator clip and solder each side to one of wire pads on upper left labelled RE.  Solder jumper wire from GND pad (lower left) to one of the grounded pads on lower edge of red LaunchPad board.  Solder jumper wire to KILL pad on lower left and connect to PB_2 pin by either stripping and inserting to bottom side female header on Launchpad or solder to top side of pin connection on blue WheeStat board.

--- WheeStatE_7d/Open_Circuit.ino
+++ WheeStatE_7d/Open_Circuit.ino
@@ -1,12 +1,11 @@
-// Open_Circuit tab
+// Open_Circuit tab, WheeStatE_7d
+// if kill switch incorporated, comment out lines 5-26, uncomment lines 27 to end
 
 void openCircuit() {
   boolean fixed;                                  
   if (oCircRun == false) {          // use existing open circuit value?
     openCirc = (dInit + dFnl)/2;
-
-  }
-  
+  }  
   PWMWrite(offset_pin,pwmRes,halfRes,pwmClock);   //added Jan 2, 2014
  PWMWrite(signal_pin,pwmRes,openCirc,pwmClock);  // set signal pin to open circuit
  readCurrent(false);        //reads current, does not Serial print
@@ -16,7 +15,6 @@ void openCircuit() {
     fixed = false;
  ++openCirc;
  }
- // while (mVi < zeroI-6) {
   else if (mVi < zeroI-6) {
     fixed = false;
  --openCirc;
@@ -25,5 +23,10 @@ void openCircuit() {
    fixed = true;
    }
  oCircRun = true;
-// }
 }
+/*
+digitalWrite(kill,HIGH);
+ PWMWrite(offset_pin,pwmRes,halfRes,pwmClock);   //added Jan 2, 2014
+ PWMWrite(signal_pin,pwmRes,halfRes,pwmClock);  // set signal pin to open circuit
+ }
+ */
